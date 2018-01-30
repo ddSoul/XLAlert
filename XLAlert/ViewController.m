0add2104e6b5ac3a491378143c18d5e634ccaa37
ddSoul <972424506@qq.com>
2018-01-30 16:38:48 +0800

    XLAlert

--- XLAlert/ViewController.m
+++ XLAlert/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "XLAlert.h"
 
 @interface ViewController ()
 
@@ -16,9 +17,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, 100, 100);
+    button.backgroundColor = [UIColor redColor];
+    [button addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)click {
+    
+    Surehandler sureHandler = ^(UIAlertAction *action) {
+        NSLog(@"...............你点击了确认");
+    };
+    Cancelhandler cancelhandler = ^(UIAlertAction *action) {
+        NSLog(@"...............你点击了取消");
+    };
+    
+    XLAlert.shareAlert.
+    title(@"提示").
+    message(@"hello\n world").
+    style(1).
+    sureButtonColor([UIColor greenColor]).
+    cancleButtonColor([UIColor orangeColor]).
+    sureButtonTitle(@"ok").
+    cancelButtonTitle(@"不ok").
+    presentViewController(self).
+    suerHandler(sureHandler).
+    cancelHandler(cancelhandler).
+    show();
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
